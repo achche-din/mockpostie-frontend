3c3498cb17c3613b2203cf845426eb500084b224
patilkrunal <patilkrunal11.kp@gmail.com>
2022-02-07 22:29:00 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-import React from 'react';
-import Create from './createEndpoint/create'
-import View from './viewEndpoints/view';
-import Login from './loginEndpoint/login';
-import { AuthProvider } from './contexts/AuthContext';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import Create from "./createEndpoint/create";
+import View from "./viewEndpoints/view";
+import Login from "./loginEndpoint/login";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Routes>
-          <Route path="/" exact element={<View/>}/>
-          <Route path="/create" exact element={<Create/>}/>
-          <Route path="/view" exact element={<View/>}/>
-          <Route path="/login" exact element={<Login/>}/>
+          <Route path="/" exact element={<View />} />
+          <Route path="/create" exact element={<Create />} />
+          <Route path="/view" exact element={<View />} />
+          <Route path="/login" exact element={<Login />} />
         </Routes>
       </Router>
     </AuthProvider>
